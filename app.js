5ea313c04ab07c3c6b81e0f517ba8b1d9dc3ae30
Dan Moore <github@mooreds.com>
2020-05-15 19:19:19 +0000

    Removed routes that were unused.

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -30,8 +28,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/test', testRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
