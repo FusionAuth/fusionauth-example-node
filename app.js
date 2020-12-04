ec4f42e0e2b0684eace356c1ee54a6c2a360ffde
Brett Guy <brett@fusionauth.io>
2020-12-04 11:03:40 -0700

    Revert "updates"
    This reverts commit 1cf0df9d0015a4c0a99b980dba40cc6e5ff94e7c, reversing
    changes made to 335b9454df459e7660cac08ca7bb39b91dc155e6.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var expressSession = require('express-session');
 var path = require('path');
 var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 
@@ -18,6 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(expressSession({resave: false, saveUninitialized: false, secret: 'fusionauth-node-example'}));
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {FusionAuthClient} = require('@fusionauth/typescript-client');
-<<<<<<< HEAD
 const clientId = '97aff5bb-e454-4e69-acd1-047ba67123af'
 const clientSecret = 'V2oqdjcS0YNJzk1zY6kLQs9oEd-FE9OFpuPJZeziuDM';
 const client = new FusionAuthClient('noapikeyneeded', 'https://team-bam.fusionauth.io');
-=======
-const clientId = 'fusionauth-application-clientid-here';
-const clientSecret = 'fusionauth-application-client-secret-here';
-const client = new FusionAuthClient('noapikeyneeded', 'http://localhost:9011');
-const pkceChallenge = require('pkce-challenge');
->>>>>>> 101f8041f44c7063edc0b655543c2b4cafc5267e
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    const stateValue = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    req.session.stateValue = stateValue;
-
-    //generate the pkce challenge/verifier dict
-    pkce_pair = pkceChallenge();
-    // Store the PKCE verifier in session
-    req.session.verifier = pkce_pair['code_verifier']
-    const challenge = pkce_pair['code_challenge']
-    res.render('index', {
-        user: req.session.user,
-        stateValue: stateValue,
-        title: 'FusionAuth Example',
-        clientId: clientId,
-        challenge: challenge
-    });
+  res.render('index', {user: req.session.user, title: 'FusionAuth Example'});
 });
 
 /* OAuth return from FusionAuth */
 router.get('/oauth-redirect', function (req, res, next) {
-<<<<<<< HEAD
   // This code stores the user in a server-side session
   console.log("here");
   client.exchangeOAuthCodeForAccessToken(req.query.code,
@@ -48,34 +26,19 @@ router.get('/oauth-redirect', function (req, res, next) {
         req.session.user = response.response.user;
       })
       .then((response) => {
-=======
-    // This code stores the user in a server-side session
-    const stateFromServer = req.query.state;
-    if (stateFromServer !== req.session.stateValue) {
-        console.log("State doesn't match. uh-oh.");
-        console.log("Saw: " + stateFromServer + ", but expected: " + req.session.stateValue);
->>>>>>> 101f8041f44c7063edc0b655543c2b4cafc5267e
         res.redirect(302, '/');
-        return;
-    }
-    client.exchangeOAuthCodeForAccessTokenUsingPKCE(req.query.code,
-        clientId,
-        clientSecret,
-        'http://localhost:3000/oauth-redirect',
-        req.session.verifier)
-        .then((response) => {
-            console.log(response.response.access_token);
-            return client.retrieveUserUsingJWT(response.response.access_token);
-        })
-        .then((response) => {
-            req.session.user = response.response.user;
-        })
-        .then((response) => {
-            res.redirect(302, '/');
-        }).catch((err) => {
-        console.log("in error");
-        console.error(JSON.stringify(err));
-    });
+      }).catch((err) => {console.log("in error"); console.error(JSON.stringify(err));});
+      
+  // This code pushes the access and refresh tokens back to the browser as secure, HTTP-only cookies
+  // client.exchangeOAuthCodeForAccessToken(req.query.code,
+  //                                        clientId,
+  //                                        clientSecret,
+  //                                        'http://localhost:3000/oauth-redirect')
+  //     .then((response) => {
+  //       res.cookie('access_token', response.response.access_token, {httpOnly: true});
+  //       res.cookie('refresh_token', response.response.refresh_token, {httpOnly: true});
+  //       res.redirect(302, '/');
+  //     }).catch((err) => {console.log("in error"); console.error(JSON.stringify(err));});
 });
 
 module.exports = router;
